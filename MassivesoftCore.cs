9d1bc135803a36cf1f4534ac3abb464dc2bf7eb3
Ezawa-Riki <128144063+Ezawa-Riki@users.noreply.github.com>
2025-10-20 23:16:23 +0800

    3rd Edition

--- MassivesoftCore.cs
+++ MassivesoftCore.cs
@@ -14,6 +14,7 @@ using System.Text.Json.Serialization;
 using System.Reflection;
 using SPTarkov.Server.Core.Loaders;
 using SPTarkov.Server.Core.Models.Eft.Hideout;
+using SPTarkov.Server.Core.Models.Enums;
 
 namespace MassivesoftCore;
 
@@ -168,6 +169,7 @@ public class MassivesoftCoreClass
             }
             else
             {
+
                 ItemAddtoTrader(traderId, details.NewId, details.TraderLoyaltyLevel ?? 1, details.BarterSchemes, details.BuyRestrictionMax ?? 1000);
             }
         }
@@ -175,11 +177,22 @@ public class MassivesoftCoreClass
         {
             if (details.CopySlotsInfo == null)
             {
-                _logger.Error($"AdvancedCreateItemFromClone: AdvancedNewItemFromCloneDetails of id {details.NewId} has invalid CopySlotsInfo!");
+                _logger.Error($"AdvancedCreateItemFromClone: AdvancedNewItemFromCloneDetails of id {details.NewId} has invalid copySlots!");
             }
             else
             {
                 ItemCopySlot(details.NewId, details.CopySlotsInfo);
+            }
+        }
+        if (details.AddSlot)
+        {
+            if (details.SlotsToAdd == null)
+            {
+                _logger.Error($"AdvancedCreateItemFromClone: AdvancedNewItemFromCloneDetails of id {details.NewId} has invalid addSlots!");
+            }
+            else
+            {
+                ItemAddSlot(details.NewId, details.SlotsToAdd);
             }
         }
         if (details.AddtoModSlots)
@@ -282,9 +295,11 @@ public class MassivesoftCoreClass
                 AssortsAddtoTrader(traderId, details.AdditionalAssortData);
             }
         }
+        if (details.ScriptedConflictingInfos != null)
+        {
+            AddScriptedConflictingList(details.NewId, details.ScriptedConflictingInfos);
+        }
 
-        ProcessItemCartridges(details.NewId);
-        ProcessItemChambers(details.NewId);
         ProcessItemSlots(details.NewId);
 
         ListLoadedItem.Add(details.NewId);
@@ -346,11 +361,14 @@ public class MassivesoftCoreClass
         }
         _logger.Error($"WeaponCopyMastering: Mastering of id {itemCopyId} not found when copying for {itemId}!");
     }
-    private void ItemCopySlot(MongoId itemId, CopySoltInfo[] copySoltInfos)
+    private void ItemCopySlot(MongoId itemId, CopySlotInfo[] copySlotInfos)
     {
-        foreach (var item in copySoltInfos)
+        var slots = DBItems![itemId].Properties!.Slots!;
+        List<Slot> newslots = new List<Slot>();
+        foreach (var item in copySlotInfos)
         {
-            string tgtSlotName = item.TgtSlotName ?? item.NewSoltName;
+            string tgtSlotName = item.TgtSlotName ?? item.NewSlotName;
+
             if (!ItemGetSlotByName(item.Id, tgtSlotName, out Slot? tgtSlot))
             {
                 _logger.Error($"ItemCopySlot: Slot {tgtSlotName} of id {item.Id} not found when adding to {itemId}!");
@@ -366,27 +384,43 @@ public class MassivesoftCoreClass
             {
                 filters!.ElementAt(0).Filter!.UnionWith(Array.ConvertAll(item.ItemsAddToSlot, tpl => (MongoId)tpl));
             }
+
             Slot newslot = new Slot
             {
-                Name = item.NewSoltName,
+                Name = item.NewSlotName,
                 Id = MongoId.Empty(),
                 Parent = itemId,
                 Properties = new SlotProperties
                 {
                     Filters = filters
-                }
+                },
+                Required = item.Required ?? tgtSlot.Required,
+                MergeSlotWithChildren = tgtSlot.MergeSlotWithChildren,
+                Prototype = tgtSlot.Prototype
             };
-            var slots = DBItems![itemId].Properties!.Slots!;
-            slots = slots.Append(newslot);
+            newslots.Add(newslot);
+        }
+        if (slots is List<Slot> slotsList)
+        {
+            slotsList.AddRange(newslots);
+        }
+
+    }
+    private void ItemAddSlot(MongoId itemId, Slot[] newSlots)
+    {
+        var slots = DBItems![itemId].Properties?.Slots;
+        if (slots is List<Slot> slotsList)
+        {
+            slotsList.AddRange(newSlots);
         }
     }
     public void WeaponAddPreset(Preset preset)
     {
-        if (DBPreset![preset.Id] == null)
+        foreach (var item in preset.Items)
         {
-            DBPreset!.TryAdd(preset.Id, preset);
+            item.ParentId = item.ParentId?.ToLower();
         }
-        else
+        if (!DBPreset!.TryAdd(preset.Id, preset))
         {
             _logger.Error($"WeaponAddPreset: Weapon preset of id {preset.Id} already exist!");
         }
@@ -404,7 +438,7 @@ public class MassivesoftCoreClass
             _logger.Error($"ItemGetSlotByName: Item of id {itemId} is null!");
             return false;
         }
-        if (!ItemHasValidSolts(tplItem))
+        if (!ItemHasValidSlots(tplItem))
         {
             return false;
         }
@@ -418,7 +452,7 @@ public class MassivesoftCoreClass
         }
         return false;
     }
-    public bool ItemHasValidSolts(TemplateItem tplItem)
+    public bool ItemHasValidSlots(TemplateItem tplItem)
     {
         if (tplItem.Type != "Item" || tplItem.Properties == null)
         {
@@ -669,9 +703,8 @@ public class MassivesoftCoreClass
             idList.Add(id);
         }
     }
-    public void ModAddtoSlot(MongoId modId, MongoId tgtId, string slotName)
+    public void ModAddtoSlot(MongoId modId, TemplateItem tplItem, string slotName)
     {
-        TemplateItem tplItem = DBItems![tgtId];
         foreach (Slot slot in tplItem.Properties!.Slots!)
         {
             if (slotName == slot.Name)
@@ -680,55 +713,46 @@ public class MassivesoftCoreClass
                 return;
             }
         }
-        _logger.Error($"ModAddtoSlot: Id {tgtId} has no slot with name {slotName}!");
+        _logger.Error($"ModAddtoSlot: Id {tplItem.Id} has no slot with name {slotName}!");
     }
     public void ProcessItemSlots(MongoId id)
     {
         TemplateItem item = DBItems![id];
-        if (!ItemHasValidSolts(item))
-        {
-            return;
-        }
+
         int indexNumber = 0;
-        foreach (Slot s in item.Properties!.Slots!)
+        if (ItemHasValidSlots(item))
         {
-            s.Parent = id;
-            s.Id = id.ToString().Substring(0, 21) + 'a' + indexNumber.ToString("X2");
+            indexNumber = 0;
+            foreach (Slot s in item.Properties!.Slots!)
+            {
+                s.Parent = id;
+                s.Id = id.ToString().Substring(0, 21) + 'a' + indexNumber.ToString("X2");
+            }
         }
-    }
-    public void ProcessItemCartridges(MongoId id)
-    {
-        TemplateItem item = DBItems![id];
-        if (!ItemHasValidCartridges(item))
+        if (ItemHasValidCartridges(item))
         {
-            return;
+            indexNumber = 0;
+            foreach (Slot s in item.Properties!.Cartridges!)
+            {
+                s.Parent = id;
+                s.Id = id.ToString().Substring(0, 21) + 'b' + indexNumber.ToString("X2");
+            }
         }
-        int indexNumber = 0;
-        foreach (Slot s in item.Properties!.Cartridges!)
+        if (ItemHasValidChambers(item))
         {
-            s.Parent = id;
-            s.Id = id.ToString().Substring(0, 21) + 'b' + indexNumber.ToString("X2");
-        }
-    }
-    public void ProcessItemChambers(MongoId id)
-    {
-        TemplateItem item = DBItems![id];
-        if (!ItemHasValidChambers(item))
-        {
-            return;
-        }
-        int indexNumber = 0;
-        foreach (Slot s in item.Properties!.Chambers!)
-        {
-            s.Parent = id;
-            s.Id = id.ToString().Substring(0, 21) + 'c' + indexNumber.ToString("X2");
+            indexNumber = 0;
+            foreach (Slot s in item.Properties!.Chambers!)
+            {
+                s.Parent = id;
+                s.Id = id.ToString().Substring(0, 21) + 'c' + indexNumber.ToString("X2");
+            }
         }
     }
     public void AddBuffs(Dictionary<string, Buff[]> Buffs)
     {
         foreach (KeyValuePair<string, Buff[]> Buff in Buffs)
         {
-            DBBuff!.Add(Buff.Key, Buff.Value);
+            DBBuff!.TryAdd(Buff.Key, Buff.Value);
         }
     }
     public void AddCrafts(HideoutProduction[] Crafts)
@@ -752,7 +776,11 @@ public class MassivesoftCoreClass
             _logger.Error($"WeaponCopyChambers: Chambers of id {cloneId} not found when copying for {id}!");
             return;
         }
-        DBItems![id].Properties!.Chambers = _cloner.Clone(cloneItemTpl.Properties!.Chambers);
+        var filter = _cloner.Clone(cloneItemTpl.Properties!.Chambers!.ElementAt(0)!.Properties!.Filters!.ElementAt(0).Filter);
+        foreach (var chamber in DBItems![id].Properties!.Chambers!)
+        {
+            chamber.Properties!.Filters!.ElementAt(0).Filter = _cloner.Clone(filter);
+        }
     }
     public void MagCopyCartridges(MongoId id, MongoId cloneId)
     {
@@ -771,7 +799,7 @@ public class MassivesoftCoreClass
             _logger.Error($"MagCopyCartridges: Cartridges of id {cloneId} not found when copying for {id}!");
             return;
         }
-        DBItems![id].Properties!.Cartridges = _cloner.Clone(cloneItemTpl.Properties!.Cartridges!);
+        DBItems![id].Properties!.Cartridges!.ElementAt(0).Properties!.Filters!.ElementAt(0).Filter = _cloner.Clone(cloneItemTpl.Properties!.Cartridges!.ElementAt(0).Properties!.Filters!.ElementAt(0).Filter);
     }
     private void AddTraderAssortFromPreset(MongoId traderId, MongoId presetId, int traderLoyaltyLevel, BarterScheme[] barterSchemes, int buyRestrictionMax = 1000, bool unlimitedCount = true, int stackObjectsCount = 9999999)
     {
@@ -881,7 +909,7 @@ public class MassivesoftCoreClass
         }
     }
 
-    private void ApplyInfoCompatibility(string tableName, HashSet<MongoId> soltFilter)
+    private void ApplyInfoCompatibility(string tableName, HashSet<MongoId> slotFilter)
     {
         if (InfoCompatibilityMapping.TryGetValue(tableName, out Dictionary<MongoId, List<MongoId>>? dict) != true)
         {
@@ -889,9 +917,9 @@ public class MassivesoftCoreClass
         }
         foreach (KeyValuePair<MongoId, List<MongoId>> addList in dict)
         {
-            if (soltFilter.Contains(addList.Key))
+            if (slotFilter.Contains(addList.Key))
             {
-                soltFilter.UnionWith(addList.Value);
+                slotFilter.UnionWith(addList.Value);
             }
         }
     }
@@ -901,6 +929,32 @@ public class MassivesoftCoreClass
         foreach (PresetToTraderInfo info in PresetToTraderInfos)
         {
             AddTraderAssortFromPreset(info.TraderId, info.PresetId, info.TraderLoyaltyLevel, info.BarterSchemes, info.BuyRestrictionMax, info.UnlimitedCount, info.StackObjectsCount);
+        }
+        string jsonFileName = "t.json";
+        _fileUtil.WriteFileAsync(System.IO.Path.Combine(pathToMod, jsonFileName), _jsonUtil.Serialize(DBTraders![DefaultTrader].Assort, true)!);
+    }
+    public void AddScriptedConflictingList(MongoId itemId, ConflictingInfos[] conflictingInfos)
+    {
+        var itemConflictingItems = DBItems![itemId].Properties!.ConflictingItems!;
+        foreach (var item in conflictingInfos)
+        {
+            string tgtSlotName = item.TgtSlotName;
+            if (!ItemGetSlotByName(item.Id, tgtSlotName, out Slot? tgtSlot))
+            {
+                _logger.Error($"AddScriptedConflictingList: Slot {tgtSlotName} of id {item.Id} not found when adding to {itemId}!");
+                continue;
+            }
+            if (tgtSlot!.Properties == null || tgtSlot!.Properties.Filters == null)
+            {
+                _logger.Error($"AddScriptedConflictingList: Slot {tgtSlotName} of id {item.Id} is invalid when adding to {itemId}!");
+                continue;
+            }
+            HashSet<MongoId> filters = _cloner.Clone(tgtSlot.Properties.Filters.ElementAt(0).Filter)!;
+            if (item.ItemsAddToSlot != null && item.ItemsAddToSlot.Length > 0)
+            {
+                filters.UnionWith(Array.ConvertAll(item.ItemsAddToSlot, tpl => (MongoId)tpl));
+            }
+            itemConflictingItems.UnionWith(filters);
         }
     }
     private record PresetToTraderInfo
@@ -945,7 +999,7 @@ public record AdvancedNewItemFromCloneDetails : NewItemFromCloneDetails
     public virtual int? TraderLoyaltyLevel { get; set; }
 
     [JsonPropertyName("barterScheme")]
-    public virtual BarterScheme[]? BarterSchemes { get; set; }
+    public virtual DeserializationBarterScheme[]? BarterSchemes { get; set; }
 
     [JsonPropertyName("buyRestrictionMax")]
     public virtual int? BuyRestrictionMax { get; set; }
@@ -969,7 +1023,15 @@ public record AdvancedNewItemFromCloneDetails : NewItemFromCloneDetails
     public virtual bool CopySlot { get; set; } = false;
 
     [JsonPropertyName("copySlots")]
-    public virtual CopySoltInfo[]? CopySlotsInfo { get; set; }
+    public virtual CopySlotInfo[]? CopySlotsInfo { get; set; }
+
+
+    //Add slots
+    [JsonPropertyName("addSlot")]
+    public virtual bool AddSlot { get; set; } = false;
+
+    [JsonPropertyName("addSlots")]
+    public virtual Slot[]? SlotsToAdd { get; set; }
 
     //Add to other items' slots
     [JsonPropertyName("addtoModSlots")]
@@ -1010,37 +1072,70 @@ public record AdvancedNewItemFromCloneDetails : NewItemFromCloneDetails
     [JsonPropertyName("magCloneCartridgeCompatibility")]
     public virtual bool MagCloneCartridgeCompatibility { get; set; } = false;
 
+    [JsonPropertyName("scriptedConflictingInfos")]
+    public virtual ConflictingInfos[]? ScriptedConflictingInfos { get; set; }
 
     //Aditional 
     [JsonPropertyName("newId")]
     public override required string NewId { get; set; }
 
 }
+public record ConflictingInfos
+{
 
-public record CopySoltInfo
+    [JsonPropertyName("id")]
+    public virtual MongoId Id { get; set; }
+    [JsonPropertyName("tgtSlotName")]
+    public virtual required string TgtSlotName { get; set; }
+    [JsonPropertyName("itemsAddtoSlot")]
+    public virtual string[]? ItemsAddToSlot { get; set; }
+}
+public record CopySlotInfo
 {
     [JsonPropertyName("id")]
     public virtual MongoId Id { get; set; }
-    [JsonPropertyName("newSoltName")]
-    public required virtual string NewSoltName { get; set; }
+    [JsonPropertyName("newSlotName")]
+    public required virtual string NewSlotName { get; set; }
     [JsonPropertyName("tgtSlotName")]
     public virtual string? TgtSlotName { get; set; }
     [JsonPropertyName("itemsAddtoSlot")]
     public virtual string[]? ItemsAddToSlot { get; set; }
+    [JsonPropertyName("required")]
+    public virtual bool? Required { get; set; }
 }
 
 public record MasterySection
 {
-    [JsonPropertyName("name")]
+    [JsonPropertyName("Name")]
     public required virtual string Name { get; set; }
 
-    [JsonPropertyName("template")]
+    [JsonPropertyName("Template")]
     public virtual string[] Templates { get; set; } = [];
 
-    [JsonPropertyName("level2")]
+    [JsonPropertyName("Level2")]
     public virtual int Level2 { get; set; }
 
-    [JsonPropertyName("level3")]
+    [JsonPropertyName("Level3")]
     public virtual int Level3 { get; set; }
 }
 
+public record DeserializationBarterScheme : BarterScheme
+{
+    private static readonly MongoId DefaultTemplate = Money.ROUBLES;
+
+    [JsonPropertyName("_tpl")]
+    public override MongoId Template
+    {
+        get => base.Template;
+        set => base.Template = value;
+    }
+
+    public DeserializationBarterScheme()
+    {
+
+        if (base.Template == default(MongoId))
+        {
+            base.Template = DefaultTemplate;
+        }
+    }
+}
